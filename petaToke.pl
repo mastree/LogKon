2a4856f6e981ea2e9dea13d6d672c105257b334e
unknown <muha.kamal@gmail.com>
2019-11-14 10:43:43 +0700

    map dinamis, move disatuin

--- petaToke.pl
+++ petaToke.pl
@@ -1,5 +1,5 @@
 :- include('util.pl').
-:- include('move.pl').
+/*:- include('move.pl').*/
  
 /*:- dynamic(deadzone/1).*/
 :- dynamic(lebarPeta/1).
@@ -12,8 +12,8 @@
  
 init_map :-
     asserta(player(1,1)),
-    random(10,20,X),
-    random(10,20,Y),
+    random(10,21,X),
+    random(10,21,Y),
     asserta(lebarPeta(X)),asserta(tinggiPeta(Y)),
     asserta(mapStatus(1)),
     generateTerrain,!.
@@ -21,6 +21,7 @@ init_map :-
 /*isMapAset(G).*/
 isTerrain('-').
 isTerrain('X').
+isWall('X').
    
 printMap(X,Y) :-
     player(X,Y), !, write('P').
@@ -36,8 +37,8 @@ generateTerrain:-
     YMax is T,
     Xp is (L+1),
     Yp is (T+1),
-    random(XMin, XMax, Gx),
-    random(YMin, YMax, Gy),
+    random(XMin, Xp, Gx),
+    random(YMin, Yp, Gy),
     asserta(gymPos(Gx,Gy)),
     asserta(terrain(I,J,'G')),
     forall(between(0,Yp,J), (
@@ -58,7 +59,11 @@ generateTerrain:-
             random(0,Panjang,NoTerrain),
             ambil(ListTerrain, NoTerrain, Terrain),
             asserta(terrain(I,J,Terrain)))*/
-            asserta(terrain(I,J,'-')));
+            random(0,10,Pick),
+            (Pick < 1,
+            asserta(terrain(I,J,'X'));
+            Pick >= 1,
+            asserta(terrain(I,J,'-'))));
             (I == Gx;
             J == Gy)
         ))
@@ -80,3 +85,74 @@ printMapAll:-
         nl
     )),
     !.
+
+/* Implementasi Move */
+
+w :-
+	player(X,Y),
+    Xnew is X,
+    Ynew is Y-1,
+    terrain(Xnew,Ynew,Isi),
+	isWall(Isi),
+	write('Jangan ke atas lagi lah coy!'),nl,!.
+
+w :-
+	retract(player(X,Y)),
+	Y > 1,
+	NewY is Y-1,
+	write([X,NewY]),nl,
+	asserta(player(X,NewY)),
+	write('Anda bergerak ke arah utara'),nl,
+	/* check ketemu pokemon atau kagak */!.
+
+a :-
+    player(X,Y),
+    Xnew is X-1,
+    Ynew is Y,
+    terrain(Xnew,Ynew,Isi),
+	isWall(Isi),
+	write('Jangan ke kiri lagi lah coy!'),nl,!.	
+
+a :-
+	retract(player(X,Y)),
+	X > 1,
+	NewX is X-1,
+	write([NewX,Y]),nl,
+	asserta(player(NewX,Y)),
+	write('Anda bergerak ke arah barat'),nl,
+	/* check ketemu pokemon atau kagak */!.
+
+s :-
+	player(X,Y),
+    Xnew is X,
+    Ynew is Y+1,
+    terrain(Xnew,Ynew,Isi),
+	isWall(Isi),
+	write('Jangan ke bawah lagi lah coy!'),nl,!.
+
+s :-
+	retract(player(X,Y)),
+	tinggiPeta(H),
+	Y < H,
+	NewY is Y+1,
+	write([X,NewY]),nl,
+	asserta(player(X,NewY)),
+	write('Anda bergerak ke arah selatan'),nl,
+	/* check ketemu pokemon atau kagak */!.
+d :-
+	player(X,Y),
+    Xnew is X+1,
+    Ynew is Y,
+    terrain(Xnew,Ynew,Isi),
+	isWall(Isi),
+	write('Jangan ke kanan lagi lah coy!'),nl,!.	
+
+d :-
+	retract(player(X,Y)),
+	lebarPeta(W),
+	X < W,
+	NewX is X+1,
+	write([NewX,Y]),nl,
+	asserta(player(NewX,Y)),
+	write('Anda bergerak ke arah timur'),nl,
+	/* check ketemu pokemon atau kagak */!.
