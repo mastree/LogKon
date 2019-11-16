83f05cbee62faaba1043f01f2f994db10e2d837b
unknown <muha.kamal@gmail.com>
2019-11-16 20:44:44 +0700

    random pokemon

--- inventory.pl
+++ inventory.pl
@@ -2,6 +2,13 @@
 :- dynamic(legendaryTokemon/7).
 :- dynamic(maxInventori/1).
 
+setNamaTipe(yuckmon, water).
+setNamaTipe(platypus, water).
+setNamaTipe(bragamon, fire).
+setNamaTipe(apimon, fire).
+setNamaTipe(daunmon, leaves).
+setNamaTipe(greenmon, leaves).
+
 currentInventoryLength(Length) :-
 	findall(A, inventori(A, _, _, _, _), ListLength),
 	length(ListLength, Length), !.
@@ -28,3 +35,30 @@ printTokemon(Nama, Tipe, _, Nyawa, _) :-
 	write(Nama), nl,
 	write('Health : '), write(Nyawa), nl,
 	write('Tipe : '), write(Tipe), nl, nl.
+
+randTokemon(Nama, Tipe, Damage, Nyawa) :-
+	findall([A,B], setNamaTipe(A,B), ListToke),
+	length(ListToke, Panjang),
+	random(0, Panjang, Ith),
+	nth0(Ith, ListToke, Dapet),
+	nth0(0, Dapet, N),
+	nth0(1, Dapet, T),
+	Nama = N,
+	Tipe = T,
+	random(0, 100, Tier),
+	(Tier<80, 
+	random(1000, 5000, Hp);
+	Tier>79,
+	Tier<93, 
+	random(5000, 15000, Hp);
+	Tier>92, 
+	random(15000, 45000, Hp)),
+	Nyawa is Hp,
+	(Tier<80, 
+	random(200, 1000, Atk);
+	Tier>79,
+	Tier<93, 
+	random(1000, 3000, Atk);
+	Tier>92, 
+	random(3000, 9000, Atk)),
+	Damage is Atk,!.
